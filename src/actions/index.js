259f0682f5ed4662861dee1d905902ed3b3c0692
alptugozates <omeralptugozates@gmail.com>
2023-11-20 21:40:50 +0300

    reset_memory import düzeltir

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const MEMORY_ADD = "MEMORY_ADD";
 export const CURRENT_MEMORY = "CURRENT_MEMORY";
-export const RESET_MEMORY = "RESET, MEMORY";
+export const RESET_MEMORY = "RESET_MEMORY";
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
